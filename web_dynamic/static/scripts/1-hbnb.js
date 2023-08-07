b492a686d0671eb70e85ed298279d1ba01b96644
NebihaDilsebo <shegolenur@hotmail.com>
2023-08-07 13:01:12 -0700

    AirBnB_clone_v4

--- web_dynamic/static/scripts/1-hbnb.js
+++ web_dynamic/static/scripts/1-hbnb.js
@@ -1,11 +1,20 @@
-document.ready(function () {
-	const amenities = {};
-	$("li input[type=checkbox]").change(function () {
-		if (this.checked) {
-			amenities[this.dataset.name] = this.dataset.id;
-		} else {
-			delete amenities[this.dataset.name];
-		}
-		$(".amenities h4").text(Object.keys(amenities).sort().join(", "));
-	});
+$(document).ready(function () {
+  const amenities = {};
+
+  // Checkbox change event handler (for both select and deselect)
+  $("li input[type=checkbox]").change(function () {
+    if (this.checked) {
+      amenities[this.dataset.name] = this.dataset.id;
+    } else {
+      delete amenities[this.dataset.name];
+    }
+    $(".amenities h4").text(Object.keys(amenities).sort().join(", "));
+  });
+
+  // Button click event handler to deselect all checkboxes
+  $("#deselect-all").click(function () {
+    $("li input[type=checkbox]").prop("checked", false);
+    amenities = {}; // Clear the amenities object
+    $(".amenities h4").text(""); // Update the displayed selected amenities text to empty
+  });
 });
