4736c2d09aad0f4991e754f6732533e58f5ee2b9
NebihaDilsebo <shegolenur@hotmail.com>
2023-09-11 05:25:55 -0700

    2-hbnb.js

--- web_dynamic/static/scripts/2-hbnb.js
+++ web_dynamic/static/scripts/2-hbnb.js
@@ -10,7 +10,7 @@ document.ready(function () {
 	});
 
 	// get status of API
-	$.getJSON("http://107.21.37.236:5001/api/v1/status/", (data) => {
+	$.getJSON("http://107.21.37.236:5003/api/v1/status/", (data) => {
 		if (data.status === "OK") {
 			$("div#api_status").addClass("available");
 		} else {
